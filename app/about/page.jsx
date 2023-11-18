600443c854b73e2d86fbb4b800115804783480d9
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2023-11-18 11:27:01 +0530

    starting new project to learn next13

---
+++ app/about/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Coinmasters</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
+        labore, cumque, saepe a, officia minima doloremque beatae debitis
+        impedit corporis eligendi facere aliquid natus laborum ea corrupti!
+        Consequuntur, eveniet porro.
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Team</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+        molestias, ad iure quam soluta ea sunt labore ipsam omnis tenetur hic
+        veniam exercitationem voluptates. Veniam cum commodi fuga quasi rerum
+        qui nesciunt ipsam dignissimos possimus.
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
